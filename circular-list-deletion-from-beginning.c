e2ffd518e579313fb50347dfc6eed5771d5fee2b
Bhumika Kanyal <79444139+Bhumika79@users.noreply.github.com>
2023-01-19 11:24:21 +0530

    Add files via upload

---
+++ circular-list-deletion-from-beginning.c
@@ -0,0 +1,111 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+  
+// Structure of a linked list node
+struct node {
+    int info;
+    struct node* next;
+};
+  
+// Pointer to last node in list
+struct node* last = NULL;
+  
+// Function to add a new node
+// at the end of the list
+void addatlast()
+{
+    // Stores number to be inserted
+    int data;
+  
+    // Initialize a new node
+    struct node* temp;
+    temp = (struct node*)malloc(sizeof(struct node));
+  
+    // Input data
+    printf("\nEnter data to be"
+           " inserted: \n");
+    scanf("%d", &data);
+  
+    // If the new node is the only
+    // node in the list
+    if (last == NULL) {
+        temp->info = data;
+        temp->next = temp;
+        last = temp;
+    }
+  
+    // Else the new node will be the
+    // last node and will contain
+    // the reference of head node
+    else {
+        temp->info = data;
+        temp->next = last->next;
+        last->next = temp;
+        last = temp;
+    }
+}
+  
+// Function to delete the first
+// element of the list
+void deletefirst()
+{
+    struct node* temp;
+  
+    // If list is empty
+    if (last == NULL)
+        printf("\nList is empty.\n");
+  
+    // Else last node now contains
+    // reference of the second node
+    // in the list because the
+    // list is circular
+    else {
+        temp = last->next;
+        last->next = temp->next;
+        free(temp);
+    }
+}
+  
+// Function to print the list
+void viewList()
+{
+    // If list is empty
+    if (last == NULL)
+        printf("\nList is empty\n");
+  
+    // Else print the list
+    else {
+        struct node* temp;
+        temp = last->next;
+        do {
+            printf("\nData = %d",
+                   temp->info);
+            temp = temp->next;
+        } while (temp != last->next);
+    }
+}
+  
+// Driver Code
+int main()
+{
+    // Initialize the list
+    addatlast();
+    addatlast();
+    addatlast();
+  
+    // Function Call
+    deletefirst();
+  
+    // Print list
+    viewList();
+  
+    return 0;
+}
