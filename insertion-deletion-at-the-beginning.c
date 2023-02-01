67638bdf70d986693854079ed6f1a903c2c6069b
Bhumika Kanyal <79444139+Bhumika79@users.noreply.github.com>
2023-02-01 19:34:32 +0530

    Update insertion-deletion-at-the-beginning.c

--- insertion-deletion-at-the-beginning.c
+++ insertion-deletion-at-the-beginning.c
@@ -1,20 +1,10 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<malloc.h>
-
 struct node{
 	int data;
 	struct node *next;
 };
-
 struct node *head = NULL;
 struct node *temp;
 main(){
@@ -33,5 +23,3 @@ main(){
 			head = head->next;
 	}
 }
-
-
