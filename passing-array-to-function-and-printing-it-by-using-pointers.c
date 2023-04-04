acfdb9d67244525929512899ff4304f66df3ff1f
Bhumika Kanyal <79444139+Bhumika79@users.noreply.github.com>
2023-04-04 11:25:13 +0530

    Update passing-array-to-function-and-printing-it-by-using-pointers.c

--- passing-array-to-function-and-printing-it-by-using-pointers.c
+++ passing-array-to-function-and-printing-it-by-using-pointers.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 void display(int a, int b, int c, int d, int e){
 }
-int main(){
+int main()
+{
     int a1[] = {0, 2, 8, 4, 12};
     display(a1[0], a1[1], a1[2], a1[3], a1[4]); /// Passing array elements to display/
     
